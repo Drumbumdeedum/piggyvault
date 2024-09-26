db8d8b7f9e4aac03b20c1338a741ca39c2162345
Daniel Szalay <sza.dan.uj@gmail.com>
2024-09-26 11:01:44 +0200

    refine add account card plus loading state of cards

--- components/LinkedAccountsList.tsx
+++ components/LinkedAccountsList.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { ghostAccounts } from "@/constants/placeholders";
 import {
   completeAccountConnection,
   listAccounts,
@@ -9,6 +8,7 @@ import { BadgeCheck, CirclePlus, LoaderPinwheel } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { Skeleton } from "./ui/skeleton";
 
 const AccountsList = () => {
   const router = useRouter();
@@ -16,12 +16,14 @@ const AccountsList = () => {
   const onClick = () => {
     router.push("/linked-accounts/country");
   };
-  const [accounts, setAccounts] = useState<Account[]>(ghostAccounts);
+  const [accounts, setAccounts] = useState<Account[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getAccounts = async () => {
       const accounts = await listAccounts();
       setAccounts(accounts!);
+      setLoading(false);
     };
     getAccounts();
   }, []);
@@ -40,23 +42,36 @@ const AccountsList = () => {
   return (
     <div className="flex gap-3">
       <div
-        className="relative border bg-gradient-to-br from-blue-500 to-green-500 text-background shadow-lg rounded-xl w-72 h-44 flex flex-col items-center justify-center cursor-pointer hover:scale-[101%] active:scale-[99%]"
+        className="group relative border bg-gradient-to-br from-blue-500 to-green-500 text-background shadow-lg rounded-xl w-72 h-44 flex flex-col items-center justify-center cursor-pointer transform will-change-transform transition-transform hover:scale-[101%] active:scale-[99%]"
         onClick={onClick}
       >
-        <div className="w-56 flex flex-col gap-2">
-          <div className="flex items-center justify-start gap-2">
-            <CirclePlus size="24" />
-            <span>
-              <h2 className="text-xl font-bold">Link a new account</h2>
-            </span>
+        {loading ? (
+          <div className="flex flex-col gap-2 h-full w-full relative p-6">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-1 w-full h-full rounded-xl">
+              <div className="text-background">Loading...</div>
+              <LoaderPinwheel size="20" className="animate-spin" />
+            </div>
+            <div className="flex-1">
+              <Skeleton className="h-6 w-32 " />
+            </div>
+            <Skeleton className="h-6 w-12" />
+            <Skeleton className="h-6 w-56" />
           </div>
-          <div>
-            <p className="text-xs text-justify">
-              To view your transaction history and balance, please link a bank
-              account.
-            </p>
-          </div>
-        </div>
+        ) : (
+          <>
+            <div className="flex flex-col gap-2 h-full w-full p-6">
+              <h3 className="font-semibold flex-1">Link a new account</h3>
+              <p className="text-sm">•••</p>
+              <p className="text-sm">Access transaction and balance data</p>
+            </div>
+            <div className="absolute top-[38%] left-1/2 transform -translate-x-1/2 -traslate-y-1/2">
+              <CirclePlus
+                size="44"
+                className="group-hover:animate-wiggle transform will-change-transform transition-transform"
+              />
+            </div>
+          </>
+        )}
         <Image
           className="absolute top-0 left-0 opacity-50"
           src="/images/lines.png"
@@ -74,31 +89,17 @@ const AccountsList = () => {
             key={index}
             className="relative border bg-gradient-to-br from-purple-500/70 to-pink-500 text-background shadow-lg rounded-xl w-72 h-44 "
           >
-            {account.currency && account.account_id.iban ? (
-              <div className="flex flex-col gap-2 h-full p-6">
-                <h3 className="font-semibold flex-1 capitalize">{cardName}</h3>
-                <div className="flex">
-                  <p className="text-sm grow">{account.currency}</p>
-                  <div className="flex items-center justify-end gap-1 text-green-400/90">
-                    <span className="text-xs">Connected</span>
-                    <BadgeCheck size="18" />
-                  </div>
+            <div className="flex flex-col gap-2 h-full p-6">
+              <h3 className="font-semibold flex-1 capitalize">{cardName}</h3>
+              <div className="flex">
+                <p className="text-sm grow">{account.currency}</p>
+                <div className="flex items-center justify-end gap-1 text-green-400/90">
+                  <span className="text-xs">Connected</span>
+                  <BadgeCheck size="18" />
                 </div>
-                <p className="text-sm">{account.account_id.iban}</p>
               </div>
-            ) : (
-              <div className="flex flex-col gap-2 h-full relative p-6">
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-2 bg-primary/50 w-full h-full rounded-xl">
-                  <div className="text-background">Loading data</div>
-                  <LoaderPinwheel size="20" className="animate-spin" />
-                </div>
-                <div className="flex-1">
-                  <h3 className="bg-background/20 h-6 w-32 rounded-sm " />
-                </div>
-                <p className="bg-background/20 h-6 w-12 rounded-sm" />
-                <p className="bg-background/20 h-6 w-56 rounded-sm" />
-              </div>
-            )}
+              <p className="text-sm">{account.account_id.iban}</p>
+            </div>
             <Image
               className="absolute top-0 left-0 opacity-50"
               src="/images/lines.png"
