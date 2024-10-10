1f22f0b2f8955c40b6d739f2908277e88dd60332
Daniel Szalay <sza.dan.uj@gmail.com>
2024-10-10 08:21:15 +0200

    fix user fetching on transactions table

--- app/(root)/transactions/page.tsx
+++ app/(root)/transactions/page.tsx
@@ -1,9 +1,14 @@
 import PageHeader from "@/components/layout/PageHeader";
 import TransactionsTable from "@/components/TransactionsTable";
+import { getLoggedInUser } from "@/lib/actions/auth.actions";
+import InitUser from "@/lib/stores/InitUser";
 
 const page = async () => {
+  const user = await getLoggedInUser();
+  if (!user) return;
   return (
     <div className="p-5">
+      <InitUser user={user} />
       <PageHeader title="Transactions" />
       <TransactionsTable />
     </div>
